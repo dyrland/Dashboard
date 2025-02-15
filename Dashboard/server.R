10e0c889b19cbab7a5c1c8cbd8930d554ee905ee
Kevin Dyrland <kevin.dyrland@tutanota.com>
2025-02-15 14:34:50 -0600

    Finished (unless QoL) the first tab!

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -8,21 +8,114 @@
 #
 
 library(shiny)
+library(xkcd)
+library(tidyverse)
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
+  #this is ugly but effective
+  #would accept help!
+  d1 <- reactiveValues(size = 1)
+  observeEvent(input$dice.go1,    {d1$size <- 1})
+  observeEvent(input$dice.go10,   {d1$size <- 10})
+  observeEvent(input$dice.go100,  {d1$size <- 100})
+  observeEvent(input$dice.go1000, {d1$size <- 1000})
+  observeEvent(input$dice.custom_button, {d1$size <- input$dice.custom_number})
+  eventReactive(input$dice.custom_number, {d1$size <- input$dice.custom_number})
+  
+  # d2 <- reactiveValues(size = 0)
+  # observeEvent(input$dice.go1,    {d2$size <- 1})
+  # observeEvent(input$dice.go10,   {d2$size <- 10})
+  # observeEvent(input$dice.go100,  {d2$size <- 100})
+  # observeEvent(input$dice.go1000, {d2$size <- 1000})
+  # observeEvent(input$dice.custom_button, {d2$size <- input$dice.custom_number})
+  # 
+  output$dice.ui <- renderUI({
+    actionButton("dice.custom_button",
+                 renderText({paste(input$dice.custom_number, "times")
+                 })
+    )
+  })
+  
+  # out <- reactive(switch(input$dice.dice_amount,
+  #                        one = sample(1:6, size = d1$size, replace = TRUE),
+  #                        two = sample(1:6, size = d1$size, replace = TRUE) +
+  #                              sample(1:6, size = d2$size, replace = TRUE)
+  # ))
+  
+  dice.dat <- reactive({
+    dice.dat.rv <-
+          tibble(x = switch(input$dice.dice_amount,
+                            one = sample(1:6, size = d1$size, replace = TRUE),
+                            two = sample(1:6, size = d1$size, replace = TRUE) +
+                                  sample(1:6, size = d1$size, replace = TRUE)
+          )
+          ) |>
+            group_by(x) |>
+            summarize(y = n()) |>
+            ungroup()
+  }) 
+  
+  # dice.dat <- tibble(x = c(1,3,5,5,6)) |>
+  #   group_by(x) |>
+  #   summarize(y = n()) |>
+  #   ungroup()
+  
+  x_range_max <- reactive({
+    case_when(input$dice.dice_amount == "one" ~ 6.5,
+              input$dice.dice_amount == "two" ~ 12.5,
+              .default = 6.5)
+  })
+  
+  output$dice.histogram <- renderPlot({
+    
+    if(input$dice.hist_type == "counts"){
+      
+      ggplot() +
+        xkcdrect(aes(xmin = x - 0.5,
+                     xmax = x + 0.5,
+                     ymin = 0,
+                     ymax = y),
+                 data = dice.dat(),
+                 fill = input$dice.color
+        ) +
+        xkcdaxis(xrange = c(0.5, x_range_max()),
+                 yrange = c(0, max(dice.dat()$y))
+        ) +
+        scale_x_continuous(breaks = switch(input$dice.dice_amount,
+                                           one = 1:6,
+                                           two = 1:12),
+                           limits = switch(input$dice.dice_amount,
+                                           one = c(0, 6.75),
+                                           two = c(0, 12.75)
+                           )
+        ) +
+        scale_y_continuous(breaks = ~round(unique(pretty(.)))) +
+        xlab("Number Rolled")
+    } else if(input$dice.hist_type == "percent"){
+      
+      ggplot() +
+        xkcdrect(aes(xmin = x - 0.5,
+                     xmax = x + 0.5,
+                     ymin = 0,
+                     ymax = y / sum(y)),
+                 data = dice.dat(),
+                 fill = input$dice.color
+        ) +
+        xkcdaxis(xrange = c(0.5, x_range_max()),
+                 yrange = c(0, max(dice.dat()$y / sum(dice.dat()$y)))
+        ) +
+        scale_x_continuous(breaks = switch(input$dice.dice_amount,
+                                          one = 1:6,
+                                          two = 1:12),
+                          limits = switch(input$dice.dice_amount,
+                                          one = c(0, 6.75),
+                                          two = c(0, 12.75)
+                          )
+        ) +
+        scale_y_continuous(labels = scales::percent) +
+        xlab("Number Rolled")
+    }
+  })
 }
+  
--- Dashboard/ui.R
+++ Dashboard/ui.R
@@ -1,45 +1,68 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# A dashboard of things I find interesting
 
 library(shiny)
 library(bslib)
 library(shinythemes)
+library(ggplot2)
+library(colourpicker)
 
 # Define UI for application that draws a histogram
 page_navbar(
-  title = "Some apps for my people",
+  title = "Some apps I found interesting to make",
   theme = bs_theme(version = 4, bootswatch = "sketchy"),
   navbar_options = navbar_options(theme = "light"),
   navset_tab(
   #Dice ----
   nav_panel(title = "Dice",
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Dice Simulator"),
     
-    "My children Play Machi Koro (https://en.wikipedia.org/wiki/Machi_Koro) and this will help them sense which cards are more valuable",
-
-    # Sidebar with a slider input for number of bins
+    tags$div("My children Play",
+     tags$a(href="https://en.wikipedia.org/wiki/Machi_Koro",
+                  "Machi Koro"),
+     "and this will help them sense which cards are more valuable;
+    I find it interesting to switch axes dynmically, as well as having 
+    to program a histogram by hand as ",
+      tags$code("xkcd"),
+      " does not have a histogram function."),
+    
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+      sidebarPanel(
+        h3("How many dice should we roll?"),
+        br(),
+        radioButtons("dice.dice_amount",
+                     label = NULL,
+                     choices = c("One" = "one",
+                                 "Two" = "two")),
+        br(),
+        h3("How many times should we roll the dice?"),
+        br(),
+        actionButton("dice.go1", "Once"),
+        actionButton("dice.go10", "10 times"),
+        actionButton("dice.go100", "100 times"),
+        actionButton("dice.go1000", "1000 times"),
+        uiOutput("dice.ui"),
+        br(),
+        h3("Should we do Counts or Percents?"),
+        radioButtons("dice.hist_type",
+                     label = NULL,
+                     choices = c("Counts" = "counts",
+                                 "Percent" = "percent")
+                     ),
+        br(),
+        h3("I want my own custom number of rolls! (Push button above after updating.)"),
+        numericInput("dice.custom_number", label = NULL,
+                     value = 0)
+      ),
+      mainPanel(
+        plotOutput("dice.histogram"),
+        br(),
+        colourInput("dice.color", "Select a color (regular names also work):",
+                    "pink", returnName = TRUE)
+      )
     )
-    ),
+  ),
+    
   #Birthdays ----
   nav_panel(title = "Birthdays",
   
