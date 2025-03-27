99959eb8100e47a9048e7135063486c2f4583989
Kevin Dyrland <kevin.dyrland@tutanota.com>
2025-03-27 08:33:34 -0500

    Add files via upload
    Newer commits! Turns out commits via linux are harder than I thought.

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -1,6 +1,13 @@
 library(shiny)
 library(xkcd)
 library(tidyverse)
+library(lubridate)
+library(english)
+library(shinyjs)
+
+dir.create('~/.fonts')
+file.copy("www/xkcd.ttf", "~/.fonts")
+system('fc-cache -f ~/.fonts')
 
 function(input, output, session) {
   #this is ugly but effective
@@ -63,7 +70,9 @@ function(input, output, session) {
                            )
         ) +
         scale_y_continuous(breaks = ~round(unique(pretty(.)))) +
-        xlab("Number Rolled")
+        xlab("Number Rolled") +
+        ylab("Count of Number Rolled")
+      
     } else if(input$dice.hist_type == "percent"){
       
       ggplot() +
@@ -86,8 +95,207 @@ function(input, output, session) {
                           )
         ) +
         scale_y_continuous(labels = scales::percent) +
-        xlab("Number Rolled")
+        xlab("Number Rolled") +
+        ylab("Proportion of Number Rolled")
     }
   })
+  
+  #some days to birthday go here
+  
+  user.date <- reactive({ymd_hms(
+                          glue(
+                            if(Sys.time() <= input$birthday.date) {
+                              year(Sys.Date()) } else {
+                              year(Sys.Date()) + 1 },
+                               as.character(month(input$birthday.date, label = TRUE)),
+                               day(input$birthday.date),
+                               " 06:30:00"
+                          )
+  )
+  })
+  
+  output$time.dif <- renderText({
+    invalidateLater(1000, session)
+    # glue(user.date()
+    # )
+    
+    glue("There are {num} {units} until your {event}! Congratulations! Happy Waiting!",
+         num = round(
+                time_length(int_flip(user.date() %--% Sys.time()),
+                            unit = input$birthday.units),
+                digits = 0),
+         units = input$birthday.units,
+         event = input$birthday.event
+    )
+  })
+  #Mondays go here!
+    #mainly because we should do calculations serve side even if they are static
+  
+  #maybe we should just make a calendar since 15 Oct 1582 to today
+  #column for date (obvi), then wday, mday, yday()
+  #we could then use group by and filters to quickly get to our year
+  
+  session$userData$time <- reactive({
+                                    ymd(
+                                      paste(input$client_year,
+                                            as.numeric(input$client_month) + 1,
+                                            input$client_day
+                                      )
+                                    )
+  })
+   
+
+  
+  data.gregor <- reactive({
+    date.data.rv <- tibble(date = seq.Date(ymd(15821015),
+                                           session$userData$time(), by = "day")) |> 
+    mutate(year = year(date),
+           month = month(date),
+           day = day(date),
+           wday = wday(date, label = TRUE),
+           true_monday = wday %in% "Mon",
+           monday = case_when(year >= 1870 & month == 1 & day == 1 ~ FALSE,
+                                #Jan 1
+                              year >= 1983 & month == 1 &
+                                day >= 15 & day <= 21 & wday %in% "Mon" ~ FALSE,
+                                #MLK
+                              year >= 1879 & month == 2 & day == 22 &
+                                year < 1971 ~ FALSE,
+                                #wash bday
+                              year >= 1971 & month == 2 &
+                                day >= 15 & day <= 21 & wday %in% "Mon" ~ FALSE,
+                                #prez day
+                              year >= 1968 & month == 5 &
+                                day >= 25 & day <= 31 & wday %in% "Mon" ~ FALSE,
+                                #memorial day
+                              year >= 2021 & month == 6 & day == 17 ~ FALSE,
+                                #juneteenth
+                              year >= 1870 & month == 7 & day == 4 ~ FALSE,
+                                #july 4th
+                              year >= 1894 & month == 9 &
+                                day >= 01 & day <= 07 & wday %in% "Mon" ~ FALSE,
+                                #labor day
+                              year >= 1971 & month == 10 &
+                                day >= 08 & day <= 14 & wday %in% "Mon" ~ FALSE,
+                                #columbus day/indifenous peopls day
+                              year >= 1938 & month == 11 & day == 11 ~ FALSE,
+                                #vets day
+                              year >= 1870 & month == 11 &
+                                day >= 22 & day <= 28 & wday %in% "Thu" ~ FALSE,
+                              #thanksgiving - black friday not a holiday
+                              year >= 1870 & month == 12 & day == 25 ~ FALSE,
+                                #xmas,
+                              wday %in% c("Sat", "Sun") ~ FALSE,
+                              .default = TRUE)
+    )
+  })
+  
+  data.week <- reactive({
+    data.gregor() |> 
+      filter(date >= floor_date(session$userData$time(), unit = "week")
+      ) |> pull(monday) |> sum()
+  })
+  
+  data.month <- reactive({
+    data.gregor() |> 
+      filter(date >= floor_date(session$userData$time(), unit = "month")
+      ) |> pull(monday) |> sum()
+  })
+  
+  data.year <- reactive({
+    data.gregor() |> 
+      filter(date >= floor_date(session$userData$time(), unit = "year")
+      ) |> pull(monday) |> sum()
+  })
+  
+  
+  # output$test <- renderPrint({data.week()
+  # })
+  
+  output$monday.week  <- renderText({
+    if(wday(session$userData$time(), label = TRUE) %in% c("Sat", "Sun")){
+    return("Today is NOT Monday!")
+    } else {
+      return(glue("Today is the {num} Monday of the week.",
+                num = ordinal(data.week())
+      )
+      )
+    }
+  })
+  
+  output$monday.month <- renderText({
+    if(wday(session$userData$time(), label = TRUE) %in% c("Sat", "Sun")){
+      return("Today is NOT Monday!")
+    } else {
+      return(glue("Today is the {num} Monday of the month.",
+                  num = ordinal(data.month())
+      )
+      )
+    }
+  })
+  output$monday.year  <- renderText({
+    if(wday(session$userData$time(), label = TRUE) %in% c("Sat", "Sun")){
+      return("Today is NOT Monday!")
+    } else {
+      return(glue("Today is the {num} Monday of the year.",
+                  num = ordinal(data.year())
+      )
+      )
+    }
+  })
+  output$monday.greg  <- renderText({
+    if(wday(session$userData$time(), label = TRUE) %in% c("Sat", "Sun")){
+      return("Today is NOT Monday!")
+    } else {
+      return(glue("Today is the {num} Monday of the Gregorian \\
+                               Calendar, established 1586 Oct 15, a Friday \\
+                               (and the first Monday).",
+                    num = data.gregor() |>
+                      pull(monday) |>
+                      sum() |> 
+                      ordinal()
+      )
+      )
+    }
+  })
+  
+  #shiny things
+  
+  observeEvent(input$shine_shiny, {
+
+    #sendConfetti()
+    insertUI(selector = "#shine_shiny",
+             where = "afterEnd",
+             #id = "song",
+             ui = tags$audio(src = "Shiny_From_Moana.mp3",
+                             type = "audio/mp3",
+                             autoplay = TRUE,
+                             controls = NA,
+                             style="display:none;")
+    )
+    
+    
+    shinyjs::disable("shine_shiny")
+    shinyjs::delay(28000, js$startConfettiInner())
+    shinyjs::delay(205000, shinyjs::enable("shine_shiny"))
+    shinyjs::delay(205000, js$stopConfettiInner())
+    #here is some code about confetti
+    #https://github.com/CodingTigerTang/shiny_spinner/blob/main/app.R
+})
+  
+
+  # observeEvent(time > (Sys.time() + 5), {
+  #   shinyjs::enable("shine_shiny")
+  # })
+  # 
+  
+  # out <- reactiveVal("Nothing")
+  # 
+  # observeEvent(input$deboop, {
+  #   out("hmmm")
+  # })
+  # 
+  # output$text <- renderText({out()})
+               
 }
   
--- Dashboard/ui.R
+++ Dashboard/ui.R
@@ -1,13 +1,25 @@
 # A dashboard of things I find interesting
-
+#devtools::install_github("ArthurData/confetti")
 library(shiny)
 library(bslib)
 library(shinythemes)
 library(ggplot2)
 library(colourpicker)
+library(lubridate)
+library(glue)
+library(shinyjs)
+#library(confetti)
+
 
 page_navbar(
+  shinyjs::useShinyjs(),
+  extendShinyjs(script = "confetti.js",functions =
+                  c("startConfettiInner",
+                    "removeConfettiInner",
+                    "stopConfettiInner")),
+  
   title = "Some apps I found interesting to make",
+  window_title = "Dyrland's Shiny Dashboard",
   theme = bs_theme(version = 4, bootswatch = "sketchy"),
   navbar_options = navbar_options(theme = "light"),
   navset_tab(
@@ -21,7 +33,11 @@ page_navbar(
     I find it interesting to switch axes dynmically, as well as having 
     to program a histogram by hand as ",
       tags$code("xkcd"),
-      " does not have a histogram function."),
+      " does not have a histogram function. Oh, and apparently I need to upload
+      the ",
+     tags$code("xkcd"),
+     " font as well as it is not native to the Shiny Servers..."
+     ),
     
     sidebarLayout(
       sidebarPanel(
@@ -54,7 +70,7 @@ page_navbar(
       mainPanel(
         plotOutput("dice.histogram"),
         br(),
-        colourInput("dice.color", "Select a color (regular names also work):",
+        colourpicker::colourInput("dice.color", "Select a color (regular names also work):",
                     "pink", returnName = TRUE)
       )
     )
@@ -63,13 +79,105 @@ page_navbar(
   #Birthdays ----
   nav_panel(title = "Birthdays",
   
-    "My children always want to know how many units until their birthday. Now they will know. I think there is legacy code on git"
-  
+    titlePanel("How long until my birthday?"),
+            
+    "My children always want to know how many units until their birthday.
+    Now they will know. I'm assuming 630am wakeup, maybe later I'll add code
+    to let you select a different time.",
+    
+    sidebarLayout(
+      sidebarPanel(
+        h3("What date are we counting to?"),
+        dateInput("birthday.date",
+                  label = "Date (in Month-Day order)",
+                  value = Sys.Date() + 1,
+                  format = "mm-dd"),
+        br(),
+        h3("What units would we like to measure in?"),
+        radioButtons("birthday.units", 
+                     label = NULL,
+                     choices = c("Months" = "months",
+                                 "Weeks" = "weeks",
+                                 "Days" = "days",
+                                 "Hours" = "hours",
+                                 "Minutes" = "minutes",
+                                 "Seconds" = "seconds"),
+                     selected = "days"
+        ),
+        br(),
+        h3("What are we counting to?"),
+        textInput("birthday.event",
+                  label = NULL,
+                  value = "Birthday"
+        )
+      ),
+      
+      mainPanel(
+        h1(textOutput("time.dif"))
+      )
+    )
   ),
   #Mondays
   nav_panel(title = "Mondays",
   
-    "One of my coworkers always lists the work days of the week in Mondays (1st monday, 2nd monday, etc). Now we can know in terms of other units as well"
+    "One of my coworkers always lists the work days of the week in Mondays (1st monday, 2nd monday, etc). Now we can know in terms of other units as well. What is a Monday? It's any working day of the week. So we have to exclude (federal) holidays",
+    
+    #h1(verbatimTextOutput("test")),
+    
+    h1(textOutput("monday.week")),
+    
+    h2(textOutput("monday.month")),
+    
+    h3(textOutput("monday.year")),
+    
+    h4(textOutput("monday.greg")),
+    
+    h5(glue("Today is the MESMERIZINGLY, WILDLY, SPELLING BE-GONE CODE Monday of your life (static text until I figure this out, or decide to cut it)")),
+    
+    HTML('<input type="text" id="client_year" name="client_year" style="display: none;"> '),
+    HTML('<input type="text" id="client_month" name="client_month" style="display: none;"> '),
+    HTML('<input type="text" id="client_day" name="client_day" style="display: none;"> '),
+    # HTML('<input type="text" id="client_time" name="client_time" style="display: none;"> '),
+    
+    
+    tags$script('
+    $(function() {
+    var time_now = new Date()
+    $("input#client_year").val(time_now.getFullYear())
+    $("input#client_month").val(time_now.getMonth())
+    $("input#client_day").val(time_now.getDate())
+    });    
+    ')
+    
+  ),
+  
+  nav_panel(title = "Shiny",
+            
+            "Because this app hasn't always been this glam",
+            
+            fluidPage(
+              fluidRow(
+                #useConfetti(),
+                tags$style(HTML("#confetti-canvas {
+                position:absolute;
+                top:0;
+                }")),
+                column(width = 4, offset = 4,
+                       div(
+                         style = "display: flex; 
+                         justify-content: center; align-items: center; height: 75vh;",
+                         tags$button(id = "shine_shiny",
+                            class = "btn action-button",
+                            tags$img(src = "https://shiny.posit.co/images/shiny-solo.png",
+                              height = "100px"
+                )
+                #textOutput("text"),
+                #actionButton("deboop", "To Be Disabled")
+            )
+            )
+              )
+              )
+            )
   )
 )
 )
